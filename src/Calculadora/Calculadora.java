3e8de5ccfd938b95fdfd36155d2ddf854d23fe20
Miguel Angel <Miguel Angel@DESKTOP-8B9CCP6>
2024-10-06 23:11:01 +0200

    commit calculadora

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -2,4 +2,24 @@ package Calculadora;
 
 public class Calculadora {
 
+	public int suma(int a, int b) {
+		return a + b;
+	}
+
+	public int resta(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplica(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		if (b != 0) {
+			return a / b;
+		} else {
+			return -1;
+		}
+	}
 }
+
